14ecb908fbf1f1b318aa8eb2e9bfe4dd4b98191c
Heather Konz <hmkonz@yahoo.com>
2023-02-22 10:00:38 -0700

    adding initial files

---
+++ app.py
@@ -0,0 +1,153 @@
+from flask import Flask, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User, Feedback
+from forms import RegisterForm, LoginForm, FeedbackForm, DeleteForm
+from sqlalchemy.exc import IntegrityError
+import pdb
+
+app=Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///feedback_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = 'abc123'
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"]=False
+
+app.app_context().push()
+
+connect_db(app)
+db.create_all()
+
+toolbar=DebugToolbarExtension(app)
+
+
+@app.route('/')
+def home_page():
+    return redirect('/register')
+
+@app.route('/register', methods =["GET", "POST"])
+def register_user():
+    form = RegisterForm()
+    # execute the following if request is a POST request AND token is valid 
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.password.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        new_user = User.register(username, password, email, first_name, last_name)
+        db.session.add(new_user)
+        # In case username is already taken, catch the error, send a message to pick another username and then render the register.html form so user can input another username
+        try:
+            db.session.commit()
+        except IntegrityError:
+            form.username.errors.append('Username taken. Please pick another')
+            return render_template('register.html', form=form)
+
+        # add username to session so it will be remembered when login again
+        session['username'] = new_user.username
+        flash("Welcome! Successfully created your account!", 'success')
+        return redirect(f'/users/{username}')
+
+    # For GET request or if token is invalid, render the template again
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods =["GET", "POST"])
+def login_user():
+    """Show login form if GET request, handle login if POST request"""
+
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
+
+    form=LoginForm()
+    # execute the following if a POST request AND token is valid 
+    if form.validate_on_submit():
+        username=form.username.data
+        password = form.password.data
+
+        # authenticate will return a user or False
+        user=User.authenticate(username, password)
+
+        if user:
+            flash(f"Welcome Back {user.username}!", "primary")
+            # add username to session when login so it will be remembered
+            session['username']=user.username #stay logged in
+            return redirect (f'/users/{username}')
+            
+        else:
+            form.username.errors = ["Invalid username/password"]
+            return render_template ('login.html', form=form)
+
+    # render template if a GET request
+    return render_template("login.html", form=form)
+
+
+@app.route('/logout')
+def logout_user():
+    """Logs user out and redirects to homepage"""
+
+    session.pop("username")
+    return redirect ('/')
+
+
+@app.route('/users/<username>')
+def show_user(username):   
+    """Show a page with info on a specific user. Must be logged in to see this page"""  
+   
+    if "username" not in session:
+        flash ("You must be logged in to view this page")                          
+        return redirect ("/")
+
+    else:
+        user=User.query.get_or_404(username)
+        form = DeleteForm()
+
+        return render_template('show_user.html', user=user, form=form)
+       
+
+@app.route('/users/<username>/feedback/add', methods=["GET", "POST"])
+def add_feedback(username):
+    """Show a form to add feedback if a GET request. Handle form if a POST request. Must be logged in to see this page"""
+
+    if "username" not in session:
+        flash ("You must be logged in to view this page")                          
+        return redirect ("/")
+
+   
+    form=FeedbackForm()
+
+    # execute the following if a POST request AND token is valid 
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+
+        feedback = Feedback(
+            title=title,
+            content=content,
+            username=username,
+        )
+
+        db.session.add(feedback)
+        db.session.commit()
+
+        return redirect(f"/users/{feedback.username}")
+
+    # else return template if a GET request or if token is invalid
+    else:
+        return render_template('feedback_form.html', form=form)
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
---
+++ models.py
@@ -0,0 +1,58 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db=SQLAlchemy()
+
+bcrypt = Bcrypt()
+
+def connect_db(app):
+    """connects to database"""
+    db.app=app
+    db.init_app(app)
+   
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(20), primary_key = True, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+
+    feedback = db.relationship("Feedback", backref="user", cascade="all,delete")
+
+    @classmethod
+    def register(cls, username, pwd, email, first_name, last_name):
+        """Register user with hashed password, email address, first and last names and return user"""
+        hashed=bcrypt.generate_password_hash(pwd)
+
+        # turn bytestring into normal (unicode utf8) string
+        hashed_utf8 = hashed.decode("utf8")
+
+        # return instance of user with username and hashed pwd
+        return cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name)
+
+    @classmethod
+    def authenticate (cls, username, pwd):
+        """Validate that user exists and their password is correct
+        Return user if valid; else return False
+        """
+        # find first user with 'username' that's in the database 
+        user=User.query.filter_by(username=username).first()
+        # If there is a user with that name and the hashed input password (pwd) matches the hashed password in the database (user.password), return the user; else return false
+        if user and bcrypt.check_password_hash(user.password, pwd):
+            # return User instance
+            return user
+        else:
+            return False
+
+
+class Feedback(db.Model):
+
+    __tablename__ = "feedback"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(db.Text, db.ForeignKey('users.username'), nullable=False)
